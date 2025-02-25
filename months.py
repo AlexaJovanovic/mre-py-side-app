78ca2b9556d4dbec24bd5d55d84de5ac1e447503
Aleksa <jovanovic.aleksa3z@gmail.com>
2025-02-25 15:23:41 +0100

    Created enum for months of the year

---
+++ months.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Months(Enum):
+	JANUAR = 0
+	FEBRAUR = 1
+	MART = 2
+	APRIL = 3
+	MAJ = 4
+	JUN = 5
+	JUL = 6
+	AVGUST = 7
+	SEPTEMBAR = 8
+	OKTOBAR = 9
+	NOVEMBAR = 10
+	DECEMBAR = 11
+
+
+
+
+
