5dc3181ccfc4387cc4da6754345c98010d672938
Aleksa <jovanovic.aleksa3z@gmail.com>
2025-02-25 16:03:38 +0100

    Added simple API for getting data about each month

--- months.py
+++ months.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import pandas as pd
 
 class Months(Enum):
 	JANUAR = 0
@@ -14,7 +15,18 @@ class Months(Enum):
 	NOVEMBAR = 10
 	DECEMBAR = 11
 
+months_df: pd.DataFrame = pd.read_csv("months.csv")
 
+days_column = 'broj dana'
+cyrilic_names_column = 'cirilica'
+average_energy_column = 'prosek(kWh/m2)'
 
+def get_number_of_days_in_a_month(month: Months) -> int:
+    return months_df[days_column][month.value]
 
+def get_months_name_cyrilic(month: Months) -> str:
+    return months_df[cyrilic_names_column][month.value]
+
+def get_average_energy_from_sun_per_m2_in_month(month: Months) -> str:
+    return months_df[average_energy_column][month.value]
 
