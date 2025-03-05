30cb5ba8fa1e7995aeef8e3d92c299dbe0dc1a46
Aleksa <jovanovic.aleksa3z@gmail.com>
2025-03-05 19:54:20 +0100

    Bug fix and added test

--- months_collection/months.py
+++ months_collection/months.py
@@ -34,11 +34,11 @@ average_energy_column = 'avg_production_kWh_m2'
 
 # BACKEND -> DB (csv file) API
 
-def db_get_all() -> list[MonthModel]:
+def __db_get_all() -> list[MonthModel]:
 	months: list[MonthModel] = []
     
 	for m in Months:
-		row = months_df[m.value]
+		row = months_df.iloc[m.value]
 		model = MonthModel(row['name_latin'], row['name_cyrillic'], row['number_of_days'], row['avg_production_kWh_m2'])
 		
 		months.append(model)
@@ -47,5 +47,11 @@ def db_get_all() -> list[MonthModel]:
 
 # FRONTEND -> BECKEND API
 
-def be_get_all() -> list[MonthModel]:
-    return db_get_all()
+def be_get_all() -> list[MonthModel]:	
+    return __db_get_all()
+
+def test():
+	m_list = be_get_all()
+
+	for m in m_list:
+		print(m)
