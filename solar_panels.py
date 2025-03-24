7233d63706905646061ed7eb169393bbb882cf85
Aleksa <jovanovic.aleksa3z@gmail.com>
2025-03-24 16:56:53 +0100

    Refactor code for solar to utilize database API

--- solar_panels.py
+++ solar_panels.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum
 from general_data_structs import OutputData
+from general_data_structs import GeneralInputData
 from general_data_structs import NOT_PROVIDED
 
 # Dve opcije za mesto instalacije solarnih panela
@@ -51,58 +52,39 @@ def calculation_for_solar_panels(
 
     return OutputData(curr_yearly_expenses, yearly_savings, payback_period, percantage_saved)
 
-def calc_for_solar_panels(
-        municipality: mp.SrbMunicipalities,
-        user_input: SolarPanelsUserInput, 
-        investment_price: float = NOT_PROVIDED, 
-        power_installed: float = NOT_PROVIDED,
-        ) -> OutputData:
+def fetch_data_and_calculate(general_input: GeneralInputData, solar_input: SolarPanelsUserInput) -> OutputData:
+    mun_name_lat = general_input.municipality_name_lat
+
+    municipality: mp.MunicipalityModel = mp.be_get_by_name_lat(mun_name_lat)
+    district: dt.DistrictModel = dt.be_get_by_name_lat(municipality.district_latin)
+
+    production_MWh_kWp : float
+    if (solar_input.location == SolarPanelLocation.LAND):
+        production_MWh_kWp = district.land_production_MWh_MWp
+    else:
+        production_MWh_kWp = district.roof_production_MWh_MWp
+
+    results: OutputData = calculation_for_solar_panels(solar_input.yearly_electricity_consumption,
+                                                        solar_input.electricity_price_per_kwh,
+                                                        solar_input.investment_price,
+                                                        solar_input.installed_power_kWp,
+                                                        production_MWh_kWp)
     
-    if (investment_price == NOT_PROVIDED or power_installed == NOT_PROVIDED):
-        # AKO SE NE SPECIFICIRA CENA IZGRADNJE I PLANIRANA SNAGA
-        # ONDA NE MOGU DA SE SRACUNAJU IZLAZNI PARAMETRI
-        # ALI MOZE DA SE PREPORUCI KOLIKA SNAGA BI ZADOVOLJILA 100% potreba
-        my_district = mp.corresponding_districts[municipality.value]
-        
-        recommended_power_to_install: float
-        
-        if (user_input.location == SolarPanelLocation.ROOF):
-            recommended_power_to_install = user_input.yearly_electricity_consumption / dt.roof_solar_production_vals[my_district.value] / 1000
-        elif (user_input.location == SolarPanelLocation.LAND):
-            recommended_power_to_install = user_input.yearly_electricity_consumption / dt.land_solar_production_vals[my_district.value] / 1000
-
-        print(f'Maskimalna snaga koja treba da se ugradi da bi se pokrili svi troskovi je: {recommended_power_to_install}kWp')
-        
-        return None
-
-    curr_yearly_expenses = user_input.yearly_electricity_consumption * user_input.electricity_price_per_kwh
-    
-    my_district = mp.corresponding_districts[municipality.value]
-    my_yearly_production : float = power_installed * dt.land_solar_production_vals[my_district.value] * 1000
-    
-    percantage_saved = my_yearly_production / user_input.yearly_electricity_consumption * 100
-    yearly_savings = my_yearly_production * user_input.electricity_price_per_kwh
-    payback_period = investment_price / yearly_savings
-
-    return OutputData(curr_yearly_expenses, yearly_savings, payback_period, percantage_saved)
-
+    return results
 
 def test():
     # Example Usage
-    user_data = SolarPanelsUserInput(
-        yearly_electricity_consumption=7000.0,
+    general_input: GeneralInputData = GeneralInputData(municipality_name_lat="Aleksandrovac")
+    solar_user_input: SolarPanelsUserInput = SolarPanelsUserInput(
+        investment_price=600000,
+        yearly_electricity_consumption=8000.0,
         electricity_price_per_kwh=10,
-        location=SolarPanelLocation.LAND
+        location=SolarPanelLocation.LAND, 
+        installed_power_kWp=5
     )
 
-
-    # TESTING
-    print(user_data)
-
-    output = calc_for_solar_panels(municipality=mp.SrbMunicipalities.ALIBUNAR, user_input=user_data)
-
-    output = calc_for_solar_panels(municipality=mp.SrbMunicipalities.ALIBUNAR, user_input=user_data, investment_price=200_000.0, power_installed=4)
-    print("\nRacunca:\n", output)
+    output: OutputData = fetch_data_and_calculate(general_input, solar_user_input)
+    print(output)
 
 if __name__ == '__main__':
 	test()
