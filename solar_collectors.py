138c4f9ebb460f2949b43b0167e64cc2861a6f97
Aleksa <jovanovic.aleksa3z@gmail.com>
2025-03-25 15:30:15 +0100

    Add struct for solar collector user input

--- solar_collectors.py
+++ solar_collectors.py
@@ -1,12 +1,24 @@
 from general_data_structs import OutputData
 from general_data_structs import NOT_PROVIDED
 import municipalities_collection.municipalities as mp
+from dataclasses import dataclass
 
+# USVOJENE KONSTANTE
 WATER_DENSITY: float = 985 # kg/m^3 at temp
 WATER_HEAT_CAPACITY: float = 4.2 # kJ/(kg*K)
 SUPPLY_WATER_TEMP: float = 10 # deg C
 DESIRED_WATER_TEMP: float = 35 # deg C
 NEEDED_WARM_WATER_VOLUME: float = 30 # liters per day per person
+
+# Informacije koje se dodatno unose za meru solarnih panela 
+@dataclass
+class SolarCollectorsUserInput:
+    investment_price: float
+    water_heating_source: str
+    energy_source_price_per_unit: float
+    number_of_people: int
+    collector_surface_area_m2: float
+    collector_efficeny: float
 
 # calc needed energy for one person
 needed_energy_in_kJ = (NEEDED_WARM_WATER_VOLUME / 1000)*WATER_DENSITY*WATER_HEAT_CAPACITY*(DESIRED_WATER_TEMP - SUPPLY_WATER_TEMP)
