7eca4ae6364b77368bcb02c65bec081128b94188
Aleksa <jovanovic.aleksa3z@gmail.com>
2025-03-27 16:49:58 +0100

    Add API function for getting efficiency coefficients based on fuel and heating type and model struct

--- fuels_and_heating_collection/fuels_and_heating_types.py
+++ fuels_and_heating_collection/fuels_and_heating_types.py
@@ -1,10 +1,17 @@
 import pandas as pd
+from dataclasses import dataclass
 
+@dataclass
+class CoefficientsModel:
+    fuel_efficency: float
+    pipe_system_efficency: float
+    regulation_efficency: float
 
 fuels_df: pd.DataFrame = pd.read_csv("fuels_and_heating_collection/goriva.csv")
 coefficients_df: pd.DataFrame = pd.read_csv("fuels_and_heating_collection/koeficijenti.csv")
 heating_types_df: pd.DataFrame = pd.read_csv("fuels_and_heating_collection/tipovi_grejanja.csv")
 
+coefficients_df.set_index(["fuel_type_cyrillic", "heating_type_cyrillic"], inplace=True)
 
 # BACKEND -> DB (csv file) API
 
@@ -28,6 +35,15 @@ def __db_get_all_heating_types_cyr() -> list[str]:
 
 	return fuel_types_cyr
 
+def __db_get_coefficents(fuel_type_cyr: str, heating_type_cyr: str) -> CoefficientsModel:
+    coeffs = coefficients_df.loc[(fuel_type_cyr, heating_type_cyr)]
+        
+    # Convert to dataclass
+    return CoefficientsModel(
+        fuel_efficency=float(coeffs["fuel_efficency"]),
+        pipe_system_efficency=float(coeffs["pipe_system_efficency"]),
+        regulation_efficency=float(coeffs["pipe_system_efficency"])
+    )
 
 # FRONTEND -> BECKEND API
 
@@ -46,3 +62,5 @@ def be_get_all_heating_types_lat() -> list[str]:
 def be_get_all_heating_types_cyr() -> list[str]:	
     return __db_get_all_heating_types_cyr()
 
+def be_get_coefficents(fuel_type_cyr: str, heating_type_cyr: str) -> CoefficientsModel:
+    return __db_get_coefficents(fuel_type_cyr, heating_type_cyr)
